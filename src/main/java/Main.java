8121fec9e1492b24d11af1ae8d498bfe24148499
Hexirp <Hexirp@users.noreply.github.com>
2018-03-08 20:36:52 +0900

    Add concept by twitter4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
+        Twitter twitter = new TwitterFactory().getInstance(new AccessToken("token", "tokenSecret"));
 
+        try (PrintStream out = new PrintStream("C:\\Users\\hexirp\\Documents\\twitter\\timeline.txt")) {
+            ResponseList<Status> statuses = twitter.getHomeTimeline(new Paging(1, 200));
+
+            out.print(statuses);
+        } catch (FileNotFoundException | TwitterException e) {
+            e.printStackTrace();
+        }
     }
 }
